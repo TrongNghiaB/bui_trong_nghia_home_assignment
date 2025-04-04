a879ea3871ad44bca44b5862aba8e0972587962a
Trong Nghia <tralfargar215@gmail.com>
2025-04-04 10:00:27 +0700

    (home_assignment): Add asssets, colors, fonts gen files

---
+++ lib/gen/colors.gen.dart
@@ -0,0 +1,150 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// MaterialColor:
+  ///   50: #FFE5E5E5
+  ///   100: #FFBFBFBF
+  ///   200: #FF959595
+  ///   300: #FF6A6A6A
+  ///   400: #FF4A4A4A
+  ///   500: #FF2A2A2A
+  ///   600: #FF252525
+  ///   700: #FF1F1F1F
+  ///   800: #FF191919
+  ///   900: #FF0F0F0F
+  static const MaterialColor cityNameTitle = MaterialColor(
+    0xFF2A2A2A,
+    <int, Color>{
+      50: Color(0xFFE5E5E5),
+      100: Color(0xFFBFBFBF),
+      200: Color(0xFF959595),
+      300: Color(0xFF6A6A6A),
+      400: Color(0xFF4A4A4A),
+      500: Color(0xFF2A2A2A),
+      600: Color(0xFF252525),
+      700: Color(0xFF1F1F1F),
+      800: Color(0xFF191919),
+      900: Color(0xFF0F0F0F),
+    },
+  );
+
+  /// MaterialColor:
+  ///   50: #FFE5E5E5
+  ///   100: #FFBFBFBF
+  ///   200: #FF959595
+  ///   300: #FF6A6A6A
+  ///   400: #FF4A4A4A
+  ///   500: #FF2A2A2A
+  ///   600: #FF252525
+  ///   700: #FF1F1F1F
+  ///   800: #FF191919
+  ///   900: #FF0F0F0F
+  static const MaterialColor currentTempTitle = MaterialColor(
+    0xFF2A2A2A,
+    <int, Color>{
+      50: Color(0xFFE5E5E5),
+      100: Color(0xFFBFBFBF),
+      200: Color(0xFF959595),
+      300: Color(0xFF6A6A6A),
+      400: Color(0xFF4A4A4A),
+      500: Color(0xFF2A2A2A),
+      600: Color(0xFF252525),
+      700: Color(0xFF1F1F1F),
+      800: Color(0xFF191919),
+      900: Color(0xFF0F0F0F),
+    },
+  );
+
+  /// MaterialColor:
+  ///   50: #FFFEFEFE
+  ///   100: #FFFCFCFD
+  ///   200: #FFFAFBFB
+  ///   300: #FFF8F9F9
+  ///   400: #FFF7F7F8
+  ///   500: #FFF5F6F7
+  ///   600: #FFF4F5F6
+  ///   700: #FFF2F3F5
+  ///   800: #FFF0F2F3
+  ///   900: #FFEEEFF1
+  static const MaterialColor defaultBackground = MaterialColor(
+    0xFFF5F6F7,
+    <int, Color>{
+      50: Color(0xFFFEFEFE),
+      100: Color(0xFFFCFCFD),
+      200: Color(0xFFFAFBFB),
+      300: Color(0xFFF8F9F9),
+      400: Color(0xFFF7F7F8),
+      500: Color(0xFFF5F6F7),
+      600: Color(0xFFF4F5F6),
+      700: Color(0xFFF2F3F5),
+      800: Color(0xFFF0F2F3),
+      900: Color(0xFFEEEFF1),
+    },
+  );
+
+  /// MaterialColor:
+  ///   50: #FFFFF0200
+  ///   100: #FFFFDA380
+  ///   200: #FFFFC252C
+  ///   300: #FFFEAA6D8
+  ///   400: #FFFE97818
+  ///   500: #FFFE85959
+  ///   600: #FFFE7D1389
+  ///   700: #FFFE7220DC
+  ///   800: #FFFE682D66
+  ///   900: #FFFD554420
+  static const MaterialColor errorBackground = MaterialColor(
+    0xFFFE85959,
+    <int, Color>{
+      50: Color(0xFFFFF0200),
+      100: Color(0xFFFFDA380),
+      200: Color(0xFFFFC252C),
+      300: Color(0xFFFEAA6D8),
+      400: Color(0xFFFE97818),
+      500: Color(0xFFFE85959),
+      600: Color(0xFFFE7D1389),
+      700: Color(0xFFFE7220DC),
+      800: Color(0xFFFE682D66),
+      900: Color(0xFFFD554420),
+    },
+  );
+
+  /// MaterialColor:
+  ///   50: #FFE9E9E9
+  ///   100: #FFC9C9C9
+  ///   200: #FFA5A5A5
+  ///   300: #FF808080
+  ///   400: #FF656565
+  ///   500: #FF4A4A4A
+  ///   600: #FF434343
+  ///   700: #FF3A3A3A
+  ///   800: #FF323232
+  ///   900: #FF222222
+  static const MaterialColor errorTitle = MaterialColor(
+    0xFF4A4A4A,
+    <int, Color>{
+      50: Color(0xFFE9E9E9),
+      100: Color(0xFFC9C9C9),
+      200: Color(0xFFA5A5A5),
+      300: Color(0xFF808080),
+      400: Color(0xFF656565),
+      500: Color(0xFF4A4A4A),
+      600: Color(0xFF434343),
+      700: Color(0xFF3A3A3A),
+      800: Color(0xFF323232),
+      900: Color(0xFF222222),
+    },
+  );
+}
