b8e8113ad22cac269b48f711393762b15f044cd4
Trong Nghia <tralfargar215@gmail.com>
2025-04-04 09:51:02 +0700

    (home_assignment): Add constants file

---
+++ lib/core/constant/constants.dart
@@ -0,0 +1,16 @@
+class URLConstants {
+  static const baseUrl = 'https://api.openweathermap.org/data/2.5/';
+}
+
+class AppString {
+  static const degreeIcon = "°";
+  static const monday = "Monday";
+  static const tuesday = "Tuesday";
+  static const wednesday = "Wednesday";
+  static const thursday = "Thursday";
+  static const friday = "Friday";
+  static const saturday = "Saturday";
+  static const sunday = "Sunday";
+  static const retry = "RETRY";
+  static const somethingWentWrongAtOurEnd = "Something went wrong at our end!";
+}
